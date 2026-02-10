ac0b9fb8006cd64288c54c7b67b2210d4df89abc
akeemu <c28isaac.kim@afacademy.af.edu>
2026-02-10 15:41:27 -0700

    Everything up to ripple-carry tests done

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,8 +49,22 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
-		wait; -- wait forever
+	   w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+           assert (w_sum = x"1" and w_Cout = '0') report "bad 0+0+1" severity error;
+       w_addends <= x"FF"; w_Cin <= '0'; wait for 10 ns;
+           assert (w_sum = x"E" and w_Cout = '1') report "bad F+F+0" severity error;
+       w_addends <= x"F0"; w_Cin <= '1'; wait for 10 ns;
+           assert (w_sum = x"0" and w_Cout = '1') report "bad 0+F+1" severity error;
+       w_addends <= x"1F"; w_Cin <= '0'; wait for 10 ns;
+           assert (w_sum = x"0" and w_Cout = '1') report "bad F+1+0" severity error;
+       w_addends <= x"78"; w_Cin <= '0'; wait for 10 ns;
+           assert (w_sum = x"F" and w_Cout = '0') report "bad 8+7+0" severity error;
+       w_addends <= x"23"; w_Cin <= '0'; wait for 10 ns;
+           assert (w_sum = x"5" and w_Cout = '0') report "bad 3+2+0" severity error;
+       w_addends <= x"AA"; w_Cin <= '1'; wait for 10 ns;
+           assert (w_sum = x"5" and w_Cout = '1') report "bad A+A+1" severity error;
+       
+	   wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
 	
